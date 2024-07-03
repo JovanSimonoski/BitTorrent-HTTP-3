53c07868d3f8fe95924852c9c36bc7705f401a50
Darijan Sekerov <darijan2002@gmail.com>
2024-07-03 22:53:40 +0200

    handshake with quic

--- app/networking.py
+++ app/networking.py
@@ -5,11 +5,50 @@ import struct
 import requests
 from app import bencode_utils
 from app import torrent_info
+from typing import Optional, cast
+import asyncio
+
+from aioquic.asyncio.client import connect
+from aioquic.asyncio.protocol import QuicConnectionProtocol
+from aioquic.quic.configuration import QuicConfiguration
+from aioquic.quic.events import QuicEvent, StreamDataReceived
+
+configuration = QuicConfiguration(is_client=True, )
+configuration.load_verify_locations("pycacert.pem")
+configuration.secrets_log_file = open('secrets', 'w')
+
+class BTClientProtocol(QuicConnectionProtocol):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._ack_waiter: Optional[asyncio.Future[bytes]] = None
+
+    async def do_handshake(self, handshake_msg: bytes) -> None:
+        # send query and wait for answer
+        stream_id = self._quic.get_next_available_stream_id()
+        self._quic.send_stream_data(stream_id, handshake_msg, end_stream=True)
+        waiter = self._loop.create_future()
+        self._ack_waiter = waiter
+        self.transmit()
+
+        return await asyncio.shield(waiter)
+
+    def quic_event_received(self, event: QuicEvent) -> None:
+        if self._ack_waiter is not None:
+            if isinstance(event, StreamDataReceived):
+                # parse answer
+                answer = event.data
+                print('server said:', answer)
+
+                # return answer
+                waiter = self._ack_waiter
+                self._ack_waiter = None
+                waiter.set_result(answer)
 
 
 def get_list_of_peers(torrentfile_data):
     tracker_url = torrentfile_data[b"announce"].decode()
-    torrent_info_hash = hashlib.sha1(bencode_utils.bencode(torrentfile_data[b"info"])).digest()
+    torrent_info_hash = hashlib.sha1(
+        bencode_utils.bencode(torrentfile_data[b"info"])).digest()
     torrent_length = torrentfile_data[b"info"][b"length"]
     peer_id = '00112233445566778899'
     port = '6881'
@@ -47,19 +86,35 @@ def perform_handshake(torrentfile_data, peer):
     tmp = wanted_peer.find(':')
     peer_ip = wanted_peer[:tmp]
     peer_port = int(wanted_peer[tmp + 1:])
-    length_of_protocol_string = struct.pack(">B", 19)
+    length_of_protocol_string = struct.pack("!B", 19)
     protocol_string = b"BitTorrent protocol"
-    reserver_bytes = b"\x00\x00\x00\x00\x00\x00\x00\x00"
-    torrent_info_hash = hashlib.sha1(bencode_utils.bencode(torrentfile_data[b"info"])).digest()
+    # reserver_bytes = b"\x00\x00\x00\x00\x00\x00\x00\x00"
+    reserver_bytes = b"\x00" * 8
+
+    torrent_info_hash = hashlib.sha1(
+        bencode_utils.bencode(torrentfile_data[b"info"])).digest()
     peer_id = b"00112233445566778899"
 
-    handshake_msg = length_of_protocol_string + protocol_string + reserver_bytes + torrent_info_hash + peer_id
+    handshake_msg = length_of_protocol_string + protocol_string + \
+        reserver_bytes + torrent_info_hash + peer_id
 
+    async def handshake(conf: QuicConfiguration, host: str, port: int) -> None:
+        print(f'initiating handshake with {host}:{port}')
+        async with connect(host, port, configuration=conf, create_protocol=BTClientProtocol) as client:
+            client = cast(BTClientProtocol, client)
+            return await client.do_handshake(handshake_msg), client
+
+
+    rcvd_msg, client = asyncio.run(handshake(
+        configuration,
+        'localhost', 9999))
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((peer_ip, peer_port))
     s.send(handshake_msg)
     received_msg = s.recv(68)
-    return s, received_msg[48:68].hex()
+    # print(received_msg, rcvd_msg)
+    # return s, received_msg[48:68].hex()
+    return s, rcvd_msg[48:68].hex()
 
 
 def receive_message(s):
@@ -75,7 +130,8 @@ def receive_message(s):
 def request_block(s, piece_number, block_number, data_length):
     requested_piece_number = int(piece_number)
     begin = block_number * 2 ** 14
-    payload = (struct.pack(">I", requested_piece_number) + struct.pack(">I", begin) + struct.pack(">I", data_length))
+    payload = (struct.pack(">I", requested_piece_number) +
+               struct.pack(">I", begin) + struct.pack(">I", data_length))
 
     message = int('6').to_bytes(1, 'big') + payload
     length_prefix = len(message).to_bytes(4, byteorder="big")
@@ -115,7 +171,8 @@ def download_piece(output_file, file_name, piece_number):
     torrent_piece_length = int(torrentfile_data[b"info"][b"piece length"])
     last_piece_length = (torrent_data_length % torrent_piece_length)
 
-    pieces_number = len(torrent_info.piece_hashes(torrentfile_data[b"info"][b"pieces"]))
+    pieces_number = len(torrent_info.piece_hashes(
+        torrentfile_data[b"info"][b"pieces"]))
     if int(piece_number) + 1 == pieces_number and last_piece_length > 0:
         total_length = last_piece_length
     else:
@@ -129,7 +186,8 @@ def download_piece(output_file, file_name, piece_number):
     piece = b""
     sha1hash = hashlib.sha1()
 
-    num_blocks = full_blocks_number + 1 if final_block_length > 0 else full_blocks_number
+    num_blocks = full_blocks_number + \
+        1 if final_block_length > 0 else full_blocks_number
 
     for i in range(num_blocks):
         current_block_size = final_block_length if i == full_blocks_number and final_block_length > 0 else block_size
@@ -137,7 +195,8 @@ def download_piece(output_file, file_name, piece_number):
         piece += block
         sha1hash.update(block)
 
-    expected_hash = torrent_info.piece_hashes(torrentfile_data[b"info"][b"pieces"])[int(piece_number)]
+    expected_hash = torrent_info.piece_hashes(
+        torrentfile_data[b"info"][b"pieces"])[int(piece_number)]
     calculated_hash = sha1hash.digest()
     if expected_hash != calculated_hash:
         raise ValueError("Calculated hash does not match expected hash")
@@ -152,12 +211,14 @@ def download_piece(output_file, file_name, piece_number):
 
 def download(output_file, torrent_file):
     torrentfile_data = bencode_utils.decode_torrentfile(torrent_file)
-    number_of_pieces = len(torrent_info.piece_hashes(torrentfile_data[b"info"][b"pieces"]))
+    number_of_pieces = len(torrent_info.piece_hashes(
+        torrentfile_data[b"info"][b"pieces"]))
 
     tmp_files = []
 
     for i in range(number_of_pieces):
-        piece_number, output_file_tmp = download_piece("/tmp/tmp-file-piece-" + str(i), torrent_file, i)
+        piece_number, output_file_tmp = download_piece(
+            "/tmp/tmp-file-piece-" + str(i), torrent_file, i)
         tmp_files.append(output_file_tmp)
     with open(output_file, "ab") as f:
         for tmp_file in tmp_files:
--- app/p2p_server.py
+++ app/p2p_server.py
@@ -15,15 +15,20 @@ class BTServerProtocol(QuicConnectionProtocol):
     def quic_event_received(self, event: QuicEvent):
         if isinstance(event, StreamDataReceived):
             # parse query
-            length = struct.unpack("!H", bytes(event.data[:2]))[0]
-            print('client said:', event.data[2 : 2 + length])
-            print('message length:', length)
-            print('full message:', event.data)
-            # query = DNSRecord.parse(event.data[2 : 2 + length])
+            if event.data == \
+            b"\023BitTorrent protocol\000\000\000\000\000\000\000\000\326\237\221\346\262\256LT$h\321\a:q\324\352\023\207\232\17700112233445566778899":
+                print('handshake attempt')
+                data = b"\023BitTorrent protocol\000\000\000\000\000\020\000\004\326\237\221\346\262\256LT$h\321\a:q\324\352\023\207\232\177-RN0.0.0-Z\365\302\317H\210\025\304\242\372\177"
+            else:
+                length = struct.unpack("!H", bytes(event.data[:2]))[0]
+                print('client said:', event.data[2:-5])
+                print('message length:', length)
+                print('full message:', event.data)
+                # query = DNSRecord.parse(event.data[2 : 2 + length])
 
-            # perform lookup and serialize answer
-            # data = query.send(args.resolver, 53)
-            data = b'Hello from server!'
+                # perform lookup and serialize answer
+                # data = query.send(args.resolver, 53)
+                data = b'Hello from server!'
 
             # send answer
             self._quic.send_stream_data(event.stream_id, data, end_stream=True)
@@ -49,7 +54,6 @@ async def main(
     port: int,
     configuration: QuicConfiguration,
     session_ticket_store: SessionTicketStore,
-    retry: bool,
 ) -> None:
     await serve(
         host,
@@ -58,51 +62,12 @@ async def main(
         create_protocol=BTServerProtocol,
         session_ticket_fetcher=session_ticket_store.pop,
         session_ticket_handler=session_ticket_store.add,
-        retry=retry,
     )
     await asyncio.Future()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="DNS over QUIC server")
-    parser.add_argument(
-        "--host",
-        type=str,
-        default="::",
-        help="listen on the specified address (defaults to ::)",
-    )
-    parser.add_argument(
-        "--port",
-        type=int,
-        default=9999,
-        help="listen on the specified port (defaults to 853)",
-    )
-    parser.add_argument(
-        "-k",
-        "--private-key",
-        default="ssl_key.pem",
-        type=str,
-        help="load the TLS private key from the specified file",
-    )
-    parser.add_argument(
-        "-c",
-        "--certificate",
-        default="ssl_cert.pem",
-        type=str,
-        # required=True,
-        help="load the TLS certificate from the specified file",
-    )
-    parser.add_argument(
-        "--retry",
-        action="store_true",
-        help="send a retry for new connections",
-    )
-    parser.add_argument(
-        "-q",
-        "--quic-log",
-        type=str,
-        help="log QUIC events to QLOG files in the specified directory",
-    )
+    parser = argparse.ArgumentParser(description="QUIC Peer server")
     parser.add_argument(
         "-v", "--verbose", action="store_true", help="increase logging verbosity"
     )
@@ -114,29 +79,21 @@ if __name__ == "__main__":
         level=logging.DEBUG if args.verbose else logging.INFO,
     )
 
-    # create QUIC logger
-    if args.quic_log:
-        quic_logger = QuicFileLogger(args.quic_log)
-    else:
-        quic_logger = None
-
     configuration = QuicConfiguration(
         is_client=False,
-        quic_logger=quic_logger,
-        verify_mode=0,
     )
 
-    configuration.load_cert_chain(args.certificate, args.private_key)
+    # configuration.load_cert_chain(args.certificate, args.private_key)
+    configuration.load_cert_chain('ssl_cert.pem', 'ssl_key.pem')
     # configuration.verify_mode = 0
 
     try:
         asyncio.run(
             main(
-                host=args.host,
-                port=args.port,
+                host='localhost',
+                port=9999,
                 configuration=configuration,
                 session_ticket_store=SessionTicketStore(),
-                retry=args.retry,
             )
         )
     except KeyboardInterrupt:
